19208457a8ebf2dc00e1df484208388e08eca1e0
Keshav Varma <keshavvarma.vegiraju@gmail.com>
2019-02-05 23:00:27 +0530

    Production ready

---
+++ js/location.js
@@ -0,0 +1,29 @@
+$("#Agra").click(function (e) { 
+    e.preventDefault();
+    $("#targetl").text("Agra");
+});
+
+$("#Ahmedabad").click(function (e) { 
+    e.preventDefault();
+    $("#targetl").text("Ahmedabad");
+});
+$("#Alleppey").click(function (e) { 
+    e.preventDefault();
+    $("#targetl").text("Alleppey");
+});
+$("#Nicobar").click(function (e) { 
+    e.preventDefault();
+    $("#targetl").text("Andaman and Nicobar");
+});
+$("#Bangalore").click(function (e) { 
+    e.preventDefault();
+    $("#targetl").text("Bangalore");
+});
+$("#Bankok").click(function (e) { 
+    e.preventDefault();
+    $("#targetl").text("Bankok");
+});
+$("#Vizag").click(function (e) { 
+    e.preventDefault();
+    $("#targetl").text("Vizag");
+});
